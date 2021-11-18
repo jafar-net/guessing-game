59c254901dbfc54e725b55210b8ea5e5edcbf9b9
Ben Mosley <bmosley1132@gmail.com>
2021-11-18 14:02:28 -0600

    final commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(@"
+           Pick your difficulty:
+           1. easy 
+           2. medium
+           3. hard
+           4. cheater");
+            var difficulty = int.Parse(Console.ReadLine());
+            int i = new int[] { 8, 6, 4, 1 }[difficulty - 1];
+
+            int secretNumber = new Random().Next(1, 101);
+
+            while (difficulty == 4 || i > 0)
+            {
+                string guessesLeft = difficulty == 4 ? "INFINITE!" : i.ToString();
+                Console.WriteLine("Guess the secret number!");
+                Console.Write($"Your Guess (Guesses Left: {guessesLeft}): ");
+                var input = int.Parse(Console.ReadLine());
+
+                if (secretNumber == input)
+                {
+                    Console.WriteLine("You Win!!");
+                    break;
+                }
+                else if (secretNumber > input)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else
+                {
+                    Console.WriteLine("Too High!");
+                };
+                i--;
+            }
+
+        }
+    }
+}
